3c0dc481ed35c5c63d6116af3db3744bcdc11d02
syntaxglng <muhamadgilang520@gmail.com>
2026-07-16 22:58:48 +0700

    Increase default timeout for HTTP GET requests

--- providers/showbox2.js
+++ providers/showbox2.js
@@ -93,7 +93,7 @@ function getFebboxToken() {
 
 // ─── HTTP CLIENT ──────────────────────────────────────────────────────────────
 function httpGet(url, headers, timeoutMs) {
-  timeoutMs = timeoutMs || 7000;
+  timeoutMs = timeoutMs || 15000;
   headers   = headers   || BASE_HEADERS;
 
   if (IS_NODE) {
